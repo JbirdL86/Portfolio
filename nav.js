270aa71d7a8300d5e0c73059332d606276911615
JbirdL86 <jgonzalez.wj@gmail.com>
2021-06-28 18:22:27 -0600

    Add code to jump to the menu's sections

--- nav.js
+++ nav.js
@@ -2,6 +2,7 @@ const navElement = document.querySelector('.nav-menu');
 const bottomMenu = document.querySelector('.navHidden');
 const openIcon = document.querySelector('.openIcon');
 const closeIcon = document.querySelector('.closeIcon');
+const lisItems = document.querySelector('ul.nav-menu > li');
 
 function open() {
   navElement.classList.replace('nav-menu', 'menuVisible');
@@ -15,3 +16,6 @@ function close() {
 
 openIcon.addEventListener('click', open);
 closeIcon.addEventListener('click', close);
+lisItems.addEventListener('click', close);
+lisItems.nextSibling.nextSibling.addEventListener('click', close);
+lisItems.nextSibling.nextSibling.nextSibling.nextSibling.addEventListener('click', close);
