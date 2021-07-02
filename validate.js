426733851e7d34bbc1597db297db9d2341603464
JbirdL86 <jgonzalez.wj@gmail.com>
2021-07-02 08:18:04 -0600

    Fix linter errors

--- validate.js
+++ validate.js
@@ -1,16 +1,14 @@
-const form = document.getElementById('contact')
 const submitButton = document.getElementById('submit');
 const email = document.getElementById('mail');
 
 email.value = 'FErnando@gmail.com';
 submitButton.addEventListener('click', () => {
-    const emailString = email.value;
+  const emailString = email.value;
 
-    console.log(emailString.toLowerCase)
-    if( emailString === emailString.toLowerCase()){
-        submitButton.setCustomValidity('');
-    } else {
-        submitButton.setCustomValidity('Email input characters must be lower case!');
-        submitButton.preventDefault();
-    }
+  if (emailString === emailString.toLowerCase()) {
+    submitButton.setCustomValidity('');
+  } else {
+    submitButton.setCustomValidity('Email input characters must be lower case!');
+    submitButton.preventDefault();
+  }
 });
